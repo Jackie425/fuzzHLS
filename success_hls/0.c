10c806cb8ce682a10ad6a200546755d366caebe1
Jackie425 <jzj@emails.bjut.edu.cn>
2023-12-28 17:56:58 +0800

    fix break

---
+++ success_hls/0.c
@@ -0,0 +1,99 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.4.0
+ * Git version: 0ec6f1b
+ * Options:   --no-packed-struct --no-embedded-assigns --no-argc --max-funcs 5 --max-block-depth 2 --max-array-dim 3 --max-expr-complexity 2 --probability-configuration ../fuzzHLS_probabilities
+ * Seed:      2054516035
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_4 = 0xAEEF20FEL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
+/* 
+ * reads : g_4
+ * writes:
+ */
+static uint8_t  func_1(void)
+{ /* block id: 0 */
+    uint8_t l_5 = 0x7AL;
+    l_5 ^= ((safe_sub_func_uint32_t_u_u(g_4, 0x45F1DBECL)) , g_4);
+    return g_4;
+}
+
+
+
+
+/* ---------------------------------------- */
+int hls_top(void)
+{
+    int print_hash_value = 0;
+    platform_main_begin();
+    crc32_gentab();
+    func_1();
+    transparent_crc(g_4, "g_4", print_hash_value);
+    return (crc32_context ^ 0xFFFFFFFFUL);
+}
+
+/************************ statistics *************************
+XXX max struct depth: 0
+breakdown:
+   depth: 0, occurrence: 2
+XXX total union variables: 0
+
+XXX non-zero bitfields defined in structs: 0
+XXX zero bitfields defined in structs: 0
+XXX const bitfields defined in structs: 0
+XXX volatile bitfields defined in structs: 0
+XXX structs with bitfields in the program: 0
+breakdown:
+XXX full-bitfields structs in the program: 0
+breakdown:
+XXX times a bitfields struct's address is taken: 0
+XXX times a bitfields struct on LHS: 0
+XXX times a bitfields struct on RHS: 0
+XXX times a single bitfield on LHS: 0
+XXX times a single bitfield on RHS: 0
+
+XXX max expression depth: 3
+breakdown:
+   depth: 1, occurrence: 2
+   depth: 3, occurrence: 1
+
+XXX total number of pointers: 0
+
+XXX times a non-volatile is read: 3
+XXX times a non-volatile is write: 1
+XXX times a volatile is read: 0
+XXX    times read thru a pointer: 0
+XXX times a volatile is write: 0
+XXX    times written thru a pointer: 0
+XXX times a volatile is available for access: 0
+XXX percentage of non-volatile access: 100
+
+XXX forward jumps: 0
+XXX backward jumps: 0
+
+XXX stmts: 2
+XXX max block depth: 0
+breakdown:
+   depth: 0, occurrence: 2
+
+XXX percentage a fresh-made variable is used: 50
+XXX percentage an existing variable is used: 50
+XXX total OOB instances added: 0
+********************* end of statistics **********************/
+
